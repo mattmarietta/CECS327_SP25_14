6d81f47ec4a3238b11c80e3521fbbb17812e5714
mattmarietta <mattmarietta24@gmail.com>
2025-05-03 16:26:10 -0700

    Updated client and server

--- client.py
+++ client.py
@@ -1,42 +1,43 @@
 #Client side code
 import socket
 #Set up a socket for the client
-client_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Gather the host and the port from the command line
 host = input("Enter the IP to connect to: ")
 port = int(input("Enter the port to connect to: "))
 
 #Next connect to the given host and port
-client_s.connect((host, port))
+client_socket.connect((host, port))
 
-valid_queries = {
+poss_queries = {
     "1": "What is the average moisture inside my kitchen fridge in the past three hours?",
     "2": "What is the average water consumption per cycle in my smart dishwasher?",
     "3": "Which device consumed more electricity among my three IoT devices (two refrigerators and a dishwasher)?"
 }
 
+#Print the queries to the user so they can choose one
 print("Choose a query:")
-for key, query in valid_queries.items():
+for key, query in poss_queries.items():
     print(f"{key}: {query}")
 
-#Get the users choice from teh query and then we will use it if the query is valid
+#Get the users choice from the query and then we will use it if the query is valid
 choice = input("Enter the number of your choice: ")
-query = valid_queries.get(choice)
+query = poss_queries.get(choice)
 
 #If the query is valid, then we can send it to the server and wait for a response
 if query:
     #Changed from message to query from o.g. code
     #Send to the server and then wait for a response
-    client_socket.send(query.encode())
+    client_socket.send(choice.encode())
     response = client_socket.recv(1024).decode()
     print("Server response:", response)
 else:
     #If it it is not valid, then print error message and give user the option again
-    print("Sorry, this query or choice is not valid.")
-    print("Please try one of the following:")
-    for query in valid_queries.values():
+    print("Sorry the one you entered is not valid.")
+    print("Please try again and choose from the following:")
+    for query in poss_queries.values():
         print(f"- {query}")
 
 #Close the client socket that was opened
-client_s.close()
+client_socket.close()
--- server.py
+++ server.py
@@ -1,26 +1,30 @@
 import socket
+import psycopg2
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+conn = psycopg2.connect("postgresql://neondb_owner:npg_E6AQYSafVP2x@ep-round-flower-a5hd2927-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require")
+cursor = conn.cursor()
 
 host = input("Enter the IP to connect to: ")
 port_num = int(input("Enter the port to connect to: "))
 
-#Bind the sockets to the host and port number and then listen for incoming connections
-#Set the socket and address to accept the incoming connection that was tehre
 server_socket.bind((host, port_num))
 server_socket.listen(5)
 incomingSocket, incomingAdress = server_socket.accept()
 
-#Need to handle data while this is still open, use while loop to keep the connection open
 while True:
-    #Get the message and decode, if it is exit then we will stop
     message = incomingSocket.recv(1024).decode()
-    print("Client:", message)
+
     if message == "exit":
         break
-    #Return the message with the uppercase version and send it back to the client
-    message = message.upper()
-    incomingSocket.send(message.encode())
 
-#Close all the sockets we opened
+    if message == '1':
+        cursor.execute("SELECT AVG((payload::json ->> 'Moisture Meter - Moisture Meter')::float) AS moisture FROM public.\"Group 14_virtual\" WHERE (payload::json ->> 'Moisture Meter - Moisture Meter')::float is not null AND time >= NOW() - INTERVAL '3 HOURS' ")
+        result = cursor.fetchone()
+        result = str(result[0])
+    message = message.upper()
+    incomingSocket.send(result.encode())
+
 server_socket.close()
 incomingSocket.close()
+cursor.close()
+conn.close()
