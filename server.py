1af4888e41b02818b8d75e36e62b8158d4252262
Matt Carranza <98849589+matthewcarranza@users.noreply.github.com>
2025-05-05 11:20:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ port_num = int(input("Enter the port to connect to: "))
 
 server_socket.bind((host, port_num))
 server_socket.listen(5)
-incomingSocket, incomingAdress = server_socket.accept()
+incomingSocket, incomingAddress = server_socket.accept()
 
 while True:
     message = incomingSocket.recv(1024).decode()
@@ -18,10 +18,54 @@ while True:
         break
 
     if message == '1':
+        # SELECT AVG((payload::json ->> 'Moisture Meter - Moisture Meter')::float) AS moisture
+        # FROM public.\"Group 14_virtual\"
+        # WHERE (payload::json ->> 'Moisture Meter - Moisture Meter')::float is not null AND time >= NOW() - INTERVAL '3 HOURS'
         cursor.execute("SELECT AVG((payload::json ->> 'Moisture Meter - Moisture Meter')::float) AS moisture FROM public.\"Group 14_virtual\" WHERE (payload::json ->> 'Moisture Meter - Moisture Meter')::float is not null AND time >= NOW() - INTERVAL '3 HOURS' ")
         result = cursor.fetchone()
-        result = str(result[0])
-    message = message.upper()
+        result = f'Average fridge moisture in the past three hours: {result[0]:.2f}% RH'
+
+    elif message == '2':
+        # SELECT AVG((payload::json ->> 'YF-S201 - WaterFlow')::float)*0.264172 AS water_flow
+        # FROM public.\"Group 14_virtual\"
+        # WHERE(payload::json ->> 'YF-S201 - WaterFlow')::float is not null
+        cursor.execute("SELECT AVG((payload::json ->> 'YF-S201 - WaterFlow')::float)*0.264172 AS water_flow FROM public.\"Group 14_virtual\" WHERE(payload::json ->> 'YF-S201 - WaterFlow')::float is not null")
+        result = cursor.fetchone()
+        result = f'Average water consumption: {result[0]:.2f} gallons'
+
+    elif message == '3':
+        # WITH Devices as (SELECT CASE
+        #     WHEN (payload::json ->> 'board_name') = 'Arduino Uno'
+        #     THEN 'Refrigerator 1'
+        #     WHEN(payload::json ->> 'board_name') = 'Arduino Uno 2'
+        #     THEN 'Refrigerator 2'
+        #     WHEN(payload::json ->> 'board_name') = 'Arduino Uno 3'
+        #     THEN 'Dishwasher'
+        #     ELSE null
+        #     END as device, CASE
+        #     WHEN(payload::json ->> 'ACS712 - Ammeter')::float is not null
+        #     THEN(payload::json ->> 'ACS712 - Ammeter')::float
+        #     WHEN(payload::json ->> 'ACS712 - Ammeter 2')::float is not null
+        #     THEN(payload::json ->> 'ACS712 - Ammeter 2')::float
+        #     WHEN(payload::json ->> 'Ammeter 3')::float is not null
+        #     THEN(payload::json ->> 'Ammeter 3')::float
+        #     ELSE null
+        #     END as ammeter
+        #     FROM public.\"Group 14_virtual\"
+        #     WHERE payload::json ->> 'board_name' IN('Arduino Uno', 'Arduino Uno 2', 'Arduino Uno 3'))
+        #
+        # SELECT device, AVG(ammeter) AS avg_energy_kWh
+        # FROM Devices
+        # GROUP BY device
+        # ORDER BY avg_energy_kWh desc
+        # LIMIT 1
+        cursor.execute("WITH Devices as (SELECT CASE WHEN (payload::json ->> 'board_name') = 'Arduino Uno' THEN 'Refrigerator 1' WHEN(payload::json ->> 'board_name') = 'Arduino Uno 2' THEN 'Refrigerator 2' WHEN(payload::json ->> 'board_name') = 'Arduino Uno 3' THEN 'Dishwasher' ELSE null END as device, CASE WHEN(payload::json ->> 'ACS712 - Ammeter')::float is not null THEN(payload::json ->> 'ACS712 - Ammeter')::float WHEN(payload::json ->> 'ACS712 - Ammeter 2')::float is not null THEN(payload::json ->> 'ACS712 - Ammeter 2')::float WHEN(payload::json ->> 'Ammeter 3')::float is not null THEN(payload::json ->> 'Ammeter 3')::float ELSE null END as ammeter FROM public.\"Group 14_virtual\" WHERE payload::json ->> 'board_name' IN('Arduino Uno', 'Arduino Uno 2', 'Arduino Uno 3')) SELECT device, AVG(ammeter) AS avg_energy_kWh FROM Devices GROUP BY device ORDER BY avg_energy_kWh desc LIMIT 1")
+        result = cursor.fetchall()[0]
+        device = result[0]
+        result = f'{device} consumed the most energy: {result[1]:.2f} kWh'
+
+    else:
+        result = 'Sorry, this query cannot be processed.'
     incomingSocket.send(result.encode())
 
 server_socket.close()
